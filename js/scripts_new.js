a7411210431f841cdc7ed23a1ea03961103ef63b
ART89-prog <dykpgilkom@mail.ru>
2022-06-12 22:58:00 +0300

    Добавил проект

---
+++ js/scripts_new.js
@@ -0,0 +1,35 @@
+$(() => {
+	
+	$('.slider').slick({
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 875,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            }
+		]
+	});
+
+
+
+
+})
