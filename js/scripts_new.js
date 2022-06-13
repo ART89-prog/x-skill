11495525ad2a05186152aeed04f2c986f8b71cb9
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2022-06-13 13:28:58 +0300

    Сделал страницу поста

--- js/scripts_new.js
+++ js/scripts_new.js
@@ -6,23 +6,30 @@ $(() => {
 		slidesToScroll: 1,
 		responsive: [
             {
-              breakpoint: 1024,
+              breakpoint: 1279,
               settings: {
                 slidesToShow: 4,
                 slidesToScroll: 1,
               }
             },
             {
-              breakpoint: 875,
+              breakpoint: 1023,
               settings: {
                 slidesToShow: 3,
                 slidesToScroll: 1
               }
             },
             {
-              breakpoint: 480,
+              breakpoint: 767,
               settings: {
                 slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 479,
+              settings: {
+                slidesToShow: 1,
                 slidesToScroll: 1
               }
             }
