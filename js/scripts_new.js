07dd3d6d595676d7700020fbba5c5197e65099fb
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2022-06-27 18:16:59 +0300

    Правки2

--- js/scripts_new.js
+++ js/scripts_new.js
@@ -38,6 +38,18 @@ $(() => {
 	});
 
 
+    if ('function' === typeof MediaPlayer) {
+		[].forEach.call(document.querySelectorAll('audio[controls]'), function (media) {
+			player = media.player = new MediaPlayer(media, {
+				svgs: {
+				    play: 'images/sprite.svg#ic_play',
+					pause: 'images/sprite.svg#ic_pause'
+				},
+			})
+		})		
+	}
+
+
     
 
     $("#js_number_student").ionRangeSlider({  
